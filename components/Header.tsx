7752bcc8fe84944136d721d05239e5c72a15ad7a
YunusEmreUslu <usluyunusemre00@gmail.com>
2025-10-02 04:00:43 +0300

    added header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import React from "react";
 import NavItems from "./NavItems";
+import UserDropdown from "./UserDropdown";
 
 const Header = () => {
   return (
@@ -19,7 +20,7 @@ const Header = () => {
         <nav className="hidden sm:block">
           <NavItems />
         </nav>
-        {/* UserDropdown */}
+        <UserDropdown />
       </div>
     </header>
   );
